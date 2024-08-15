abd7b1b72b71b10070607bfb585141fcda6d8552
brunovTavares <bruno.tavares@wallee.com>
2024-08-15 17:13:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
-{
-    "applications": [
-        {
-            "applicationId": "com.wallee.android.pinpad",
-            "configuration": {
-                "on-home-screen": false,
-                "custom": []
-            },
-            "downloadUrl": "https://android-bundler.wallee.com/upload/apps/com.wallee.android.pinpad/120009/paydroid.apk",
-            "version": "120009"
-        }
-    ],
-    "configuration": {},
-    "last-changed": "2024-08-15T15:04:24+00:00",
-    "reportUrl": "https://android-bundler.wallee.com/report.php"
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <h1>JSON Data</h1>
+    <pre id="json-data"></pre>
+
+    <script>
+        const jsonData = {
+            "applications": [
+                {
+                    "applicationId": "com.wallee.android.pinpad",
+                    "configuration": {
+                        "on-home-screen": false,
+                        "custom": []
+                    },
+                    "downloadUrl": "https://android-bundler.wallee.com/upload/apps/com.wallee.android.pinpad/120009/paydroid.apk",
+                    "version": "120010"
+                }
+            ],
+            "configuration": {},
+            "last-changed": "2024-08-15T15:04:24+00:00",
+            "reportUrl": "https://android-bundler.wallee.com/report.php"
+        };
+
+        document.getElementById('json-data').textContent = JSON.stringify(jsonData, null, 4);
+    </script>
+</body>
+</html>
